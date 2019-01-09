83a15934c801d27cca9e71e6aec4aa327ec3162c
Prasanta Singh <singh.prasanta@gmail.com>
2019-01-09 21:26:42 +0530

    define Block struct for leader

--- pos/bc_pos.go
+++ pos/bc_pos.go
@@ -7,3 +7,14 @@ type Node struct {
 	Address string // Address of Node
 }
 
+// Block will a simple leader record
+type Block struct {
+	Data          string // Will hold the Data Map
+	Hash          string // Will have the Hast of current Block
+	PrevHash      string // Parent Block Hash
+	Timestamp     int64  // Block creation timestamp
+	ValidatorAddr string // Address of Validator Node
+	ValidatorId   int    // Added to identify quickly(for human) - Not Required in reality
+	// PoW        int    // PoW or nonce to match the Hash Difficulty or Pattern
+}
+
