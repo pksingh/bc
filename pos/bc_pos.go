6cce341d4d68ed9b6e571299bac1150083925514
Prasanta Singh <singh.prasanta@gmail.com>
2019-03-17 20:28:43 +0530

    def ValidateBlockchain() to validate BC

--- pos/bc_pos.go
+++ pos/bc_pos.go
@@ -123,6 +123,37 @@ func (n PoSNetwork) GenerateNewBlock(Validator *Node, data string) ([]*Block, *B
 	return n.Blockchain, newBlock, nil
 }
 
+// ValidateBlockchain() : It will validate the BC
+func (n PoSNetwork) ValidateBlockchain() error {
+	if len(n.Blockchain) <= 1 {
+		return nil
+	}
+
+	currBlockIdx := len(n.Blockchain) - 1
+	prevBlockIdx := len(n.Blockchain) - 2
+
+	for prevBlockIdx >= 0 {
+		currBlock := n.Blockchain[currBlockIdx]
+		prevBlock := n.Blockchain[prevBlockIdx]
+		if currBlock.PrevHash != prevBlock.Hash {
+			return errors.New("blockchain has inconsistent hashes")
+		}
+
+		//if currBlock.Timestamp <= prevBlock.Timestamp {
+		//if currBlock.Timestamp < prevBlock.Timestamp {
+		// if currBlock.Timestamp.After(prevBlock.Timestamp) {
+		// 	return errors.New("blockchain has inconsistent timestamps")
+		// }
+
+		if NewBlockHash(prevBlock) != currBlock.Hash {
+			return errors.New("blockchain has inconsistent hash generation")
+		}
+		currBlockIdx--
+		prevBlockIdx--
+	}
+	return nil
+}
+
 // ValidateNewBlock() : Will validate the Newly added block
 func (n PoSNetwork) ValidateNewBlock(newBlock *Block) error {
 	if n.BlockHead.Hash != newBlock.PrevHash {
